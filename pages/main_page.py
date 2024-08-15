c61d7ff31136908a28ae75eb4d810d1e8b71bb5c
Uladzislau Chabukin <vladchebukin@yandex.ru>
2024-08-15 20:28:59 +0300

    Fixed 3rd comment

--- pages/main_page.py
+++ pages/main_page.py
@@ -8,22 +8,9 @@ class MainPage(BasePage):
     SCOOTER_IMG = (By.XPATH, ".//img[contains(@src,'scooter.png')]")
     HOME_PAGE_DIV = (By.XPATH, ".//div[contains(@class,'HomePage')]")
     FOOTER = (By.XPATH, ".//div[contains(@class,'FourPart')]")
-    PAYMENT_QUESTION = (By.XPATH, ".//div[contains(@id, 'heading-0')]")
-    PAYMENT_ANSWER = (By.XPATH, ".//div[contains(@id, 'panel-0')]")
-    QUANTITY_QUESTION = (By.XPATH, ".//div[contains(@id, 'heading-1')]")
-    QUANTITY_ANSWER = (By.XPATH, ".//div[contains(@id, 'panel-1')]")
-    CALCULATION_RENTAL_QUESTION = (By.XPATH, ".//div[contains(@id, 'heading-2')]")
-    CALCULATION_RENTAL_ANSWER = (By.XPATH, ".//div[contains(@id, 'panel-2')]")
-    ORDER_FOR_TODAY_QUESTION = (By.XPATH, ".//div[contains(@id, 'heading-3')]")
-    ORDER_FOR_TODAY_ANSWER = (By.XPATH, ".//div[contains(@id, 'panel-3')]")
-    RENEW_OR_RETURN_QUESTION = (By.XPATH, ".//div[contains(@id, 'heading-4')]")
-    RENEW_OR_RETURN_ANSWER = (By.XPATH, ".//div[contains(@id, 'panel-4')]")
-    CHARGING_QUESTION = (By.XPATH, ".//div[contains(@id, 'heading-5')]")
-    CHARGING_ANSWER = (By.XPATH, ".//div[contains(@id, 'panel-5')]")
-    CANCEL_ORDER_QUESTION = (By.XPATH, ".//div[contains(@id, 'heading-6')]")
-    CANCEL_ORDER_ANSWER = (By.XPATH, ".//div[contains(@id, 'panel-6')]")
-    MKAD_QUESTION = (By.XPATH, ".//div[contains(@id, 'heading-7')]")
-    MKAD_ANSWER = (By.XPATH, ".//div[contains(@id, 'panel-7')]")
+    QUESTION = (By.XPATH, ".//div[contains(@id, 'heading-{0}')]")
+    ANSWER = (By.XPATH, ".//div[contains(@id, 'panel-{0}')]")
+
     BUTTON_ORDER_IN_MIDDLE = (By.XPATH, ".//div[contains(@class,'FinishButton')]//button")
 
     def __init__(self, driver):
@@ -42,12 +29,19 @@ class MainPage(BasePage):
         self._open_the_page()
         self.go_to_the_footer()
 
+    @allure.step('Нажать на соответствующий вопрос')
+    def click_on_question(self, variable):
+        question_locator = self._modify_locator(self.QUESTION, variable)
+        self._wait_and_click_on_element(question_locator)
+
+    @allure.step('Нажать на кнопку "Заказать" в центре')
+    def click_on_order_button_in_middle(self):
+        self._go_to_element(self.BUTTON_ORDER_IN_MIDDLE)
+        self._wait_and_click_on_element(self.BUTTON_ORDER_IN_MIDDLE)
+
     @allure.step('Проверка соответствия текста ответа')
-    def check_text_answer_of_question(self, answer_locator, expected_answer):
+    def check_text_answer_of_question(self, variable, expected_answer):
+        answer_locator = self._modify_locator(self.ANSWER, variable)
         actual_answer = self._wait_and_find_element(answer_locator).text
 
         assert expected_answer == actual_answer, f'Ожидаемый ответ {expected_answer}, фактический — f{actual_answer}'
-
-    @allure.step('Нажать на соответствующий вопрос')
-    def click_on_question(self):
-        self._wait_and_click_on_element(self.PAYMENT_QUESTION)
--- tests/test_base_page.py
+++ tests/test_base_page.py
@@ -1,94 +1,17 @@
 import allure
+import pytest
+from data import QuestionData
 from pages.main_page import MainPage
 
 
 class TestFooter:
-    @allure.title('Проверка вопроса "Сколько это стоит? И как оплатить?"')
-    @allure.description('При нажатии на вопрос открывается соответствующий текст — "Сутки — 400 рублей. Оплата курьеру \
-                                                                                            — наличными или картой."')
-    def test_payment_question(self, driver):
+    @pytest.mark.parametrize(QuestionData.param, QuestionData.value)
+    @allure.title(f'Проверка вопроса')
+    @allure.description('При нажатии на вопрос открывается соответствующий текст')
+    def test_question(self, driver, number, expected_result):
         base_page = MainPage(driver)
         base_page.open_the_the_page_and_go_to_the_footer()
 
-        base_page.click_on_question()
+        base_page.click_on_question(number)
 
-        base_page.check_text_answer_of_payment_question()
-
-    @allure.title('Проверка вопроса "Хочу сразу несколько самокатов! Так можно?"')
-    @allure.description('При нажатии на вопрос открывается соответствующий текст — "Пока что у нас так: один заказ \
-        — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим."')
-    def test_quantity_question(self, driver):
-        base_page = MainPage(driver)
-        base_page.open_the_the_page_and_go_to_the_footer()
-
-        base_page.click_on_quantity_question()
-
-        base_page.check_text_answer_of_quantity_question()
-
-    @allure.title('Проверка вопроса "Как рассчитывается время аренды?"')
-    @allure.description('При нажатии на вопрос открывается соответствующий текст — "Допустим, вы оформляете заказ на '
-                        '8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, '
-                        'когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда '
-                        'закончится 9 мая в 20:30."')
-    def test_calculation_rental_question(self, driver):
-        base_page = MainPage(driver)
-        base_page.open_the_the_page_and_go_to_the_footer()
-
-        base_page.click_on_calculation_rental_question()
-
-        base_page.check_text_answer_of_calculation_rental_question()
-
-    @allure.title('Проверка вопроса "Можно ли заказать самокат прямо на сегодня?"')
-    @allure.description('При нажатии на вопрос открывается соответствующий текст — "Только начиная с завтрашнего дня. \
-                                                                                        Но скоро станем расторопнее."')
-    def test_order_for_today_question(self, driver):
-        base_page = MainPage(driver)
-        base_page.open_the_the_page_and_go_to_the_footer()
-
-        base_page.click_on_order_for_today_question()
-
-        base_page.check_text_answer_of_order_for_today_question()
-
-    @allure.title('Проверка вопроса "Можно ли продлить заказ или вернуть самокат раньше?"')
-    @allure.description('При нажатии на вопрос открывается соответствующий текст — "Пока что нет! Но если что-то '
-                        'срочное — всегда можно позвонить в поддержку по красивому номеру 1010."')
-    def test_renew_or_return_question(self, driver):
-        base_page = MainPage(driver)
-        base_page.open_the_the_page_and_go_to_the_footer()
-
-        base_page.click_on_renew_or_return_question()
-
-        base_page.check_text_answer_of_renew_or_return_question()
-
-    @allure.title('Проверка вопроса "Вы привозите зарядку вместе с самокатом?"')
-    @allure.description('При нажатии на вопрос открывается соответствующий текст — "Самокат приезжает к вам с полной '
-                        'зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. '
-                        'Зарядка не понадобится."')
-    def test_charging_question(self, driver):
-        base_page = MainPage(driver)
-        base_page.open_the_the_page_and_go_to_the_footer()
-
-        base_page.click_on_charging_question()
-
-        base_page.check_text_answer_of_charging_question()
-
-    @allure.title('Проверка вопроса "Можно ли отменить заказ?"')
-    @allure.description('При нажатии на вопрос открывается соответствующий текст — "Да, пока самокат не привезли. '
-                        'Штрафа не будет, объяснительной записки тоже не попросим. Все же свои."')
-    def test_cancel_order_question(self, driver):
-        base_page = MainPage(driver)
-        base_page.open_the_the_page_and_go_to_the_footer()
-
-        base_page.click_on_cancel_order_question()
-
-        base_page.check_text_answer_of_cancel_order_question()
-
-    @allure.title('Проверка вопроса "Я жизу за МКАДом, привезёте?"')
-    @allure.description('Да, обязательно. Всем самокатов! И Москве, и Московской области."')
-    def test_mkad_question(self, driver):
-        base_page = MainPage(driver)
-        base_page.open_the_the_page_and_go_to_the_footer()
-
-        base_page.click_on_mkad_question()
-
-        base_page.check_text_answer_of_mkad_question()
+        base_page.check_text_answer_of_question(number, expected_result)
